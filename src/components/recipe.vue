5e053bd7ccfd56a02ea1f3fd1781bb938bb61b87
mindcool24 <mindcool24@jsociety.xyz>
2022-11-27 20:19:38 -0700

    Ran lint

--- src/components/recipe.vue
+++ src/components/recipe.vue
@@ -1,21 +1,24 @@
 <script setup>
-const props = defineProps({
-	recipe: Object
-})
-
+  const props = defineProps({
+    recipe: Object,
+  });
 </script>
 
 <template>
-	<h1>ID: {{recipe.id}}</h1>
-	<h2>Title: {{recipe.title}}</h2>
-	<p>Author: {{recipe.author}}</p>
-	<h3>Ingredients:</h3>
-	<ul>
-		<li v-for="item of JSON.parse(recipe.ingredients)['ingredients']">{{item["amount"]}} - {{item["name"]}}</li>
-	</ul>
-	<h3>Instructions:</h3>
-	<ol>
-		<li v-for="item of JSON.parse(recipe.instructions)['instructions']">{{item}}</li>
-	</ol>
-	<p>Notes: {{recipe.notes}}</p>
+  <h1>ID: {{ recipe.id }}</h1>
+  <h2>Title: {{ recipe.title }}</h2>
+  <p>Author: {{ recipe.author }}</p>
+  <h3>Ingredients:</h3>
+  <ul>
+    <li v-for="item of JSON.parse(recipe.ingredients)['ingredients']">
+      {{ item["amount"] }} - {{ item["name"] }}
+    </li>
+  </ul>
+  <h3>Instructions:</h3>
+  <ol>
+    <li v-for="item of JSON.parse(recipe.instructions)['instructions']">
+      {{ item }}
+    </li>
+  </ol>
+  <p>Notes: {{ recipe.notes }}</p>
 </template>
