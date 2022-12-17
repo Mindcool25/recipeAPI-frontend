7b98c76bdab822af55252bba6cb907bff47c56c8
Mindcool25 <mindcool24@jsociety.xyz>
2022-12-16 22:14:06 -0700

    added routing

---
+++ src/router.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Recipe from './views/RecipeView.vue'
+import Submit from './views/SubmitView.vue'
+const routes = [
+  {
+    path: '/recipe/:id',
+    name: 'Recipe',
+    component: Recipe
+  },
+  {
+    path: '/submit',
+    name: 'Submit',
+    component: Submit
+  },
+]
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+export default router
