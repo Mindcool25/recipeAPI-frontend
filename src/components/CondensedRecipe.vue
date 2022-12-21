ba98e4bdc8d7c119d25778b758507a76402e6f78
Mindcool25 <mindcool24@jsociety.xyz>
2022-12-21 13:42:14 -0700

    Compact recipes now link to recipe

--- src/components/CondensedRecipe.vue
+++ src/components/CondensedRecipe.vue
@@ -1,14 +1,16 @@
-
 <script setup>
   const props = defineProps({
     recipe: Object,
   });
+
 </script>
 
 <template>
+  <router-link :to="`/recipe/${recipe._id.$oid}`">
   <div style="border:2px solid black;">
     <h2>Title: {{ recipe.title }}</h2>
     <p>Author: {{ recipe.author }}</p>
     <p>Notes: {{ recipe.notes }}</p>
   </div>
+  </router-link>
 </template>
